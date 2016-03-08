7c3971903c4c8e622ee5ffa01194c4733c6a50a5
UPC <user@user.(none)>
2016-03-08 09:53:27 +0100

    added action description to actuate script

--- servioticy/actuate.sh
+++ servioticy/actuate.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-[ $# -ne 3 ] && { echo "Usage: $0 <API_KEY> <Service Object ID> <action>"; exit 1; } 
+[ $# -ne 3 ] && { echo "Usage: $0 <API_KEY> <Service Object ID> <action file>"; exit 1; } 
 
 key=$(cat $1)
 ide=$(cat $2)
+
+action_params=()
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    action_params+=("$line")
+done < "$3"
 
 cr=$'\r'
 ide="${ide%$cr}"
 
 curl -i -X POST -H "Content-Type: text/plain" \
--H "Authorization: ${key}" http://api.servioticy.com/${ide}/actuations/$3
+-d ${action_params[1]} \
+-H "Authorization: ${key}" http://api.servioticy.com/${ide}/actuations/${action_params[0]}
